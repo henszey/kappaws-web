d0b1a491a9e915fef8b04762ba32bb7e9ac6f1b2
Ryan Henszey <henszey@gmail.com>
2015-08-11 01:03:35 -0500

    root url

--- src/main/java/com/timelessname/server/web/HomeController.java
+++ src/main/java/com/timelessname/server/web/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-  @RequestMapping("/test")
+  @RequestMapping("/")
   String index() {
       return "index";
   }
